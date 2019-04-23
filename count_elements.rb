da2fc4ac5d2f254f88a5f99d947f2e926d8d9149
Won Kim <wonsae.kim@gmail.com>
2019-04-23 15:14:04 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,15 @@
+require 'pry'
 def count_elements(array)
   # code goes here
+  result = {}
+  array.each do |animal|
+    if result.has_key?(animal)
+      result[animal] += 1
+    else
+      result[animal] = 1
+      #binding.pry
+    end
+  end
+  result
 end
  
