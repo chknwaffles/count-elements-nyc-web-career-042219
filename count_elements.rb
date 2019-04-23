ec1c7fa1fcd62a5cd24fac2281958559ab867932
Won Kim <wonsae.kim@gmail.com>
2019-04-23 15:10:18 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -2,7 +2,7 @@ def count_elements(array)
   # code goes here
   result = {}
   array.each do |animal|
-    result[animal] = 1 unless result.has_key?(animal)
+    result = {animal => 1} unless result.has_key?(animal)
     result[animal] += 1 if result.has_key?(animal)
   end
 end
